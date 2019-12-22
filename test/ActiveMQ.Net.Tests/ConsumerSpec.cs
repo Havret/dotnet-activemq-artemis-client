31d4e6295d8c297adea8d68daa8c765e8c9bf656
Havret <h4vret@gmail.com>
2019-12-22 20:52:58 +0100

    Consumer should attach to specified address

--- test/ActiveMQ.Net.Tests/ConsumerSpec.cs
+++ test/ActiveMQ.Net.Tests/ConsumerSpec.cs
@@ -11,7 +11,7 @@ namespace ActiveMQ.Net.Tests
     public class ConsumerSpec
     {
         private readonly string _address = "amqp://guest:guest@localhost:15672";
-        
+
         [Fact]
         public async Task Should_create_and_close_consumer()
         {
@@ -41,7 +41,34 @@ namespace ActiveMQ.Net.Tests
             Assert.True(consumerAttached.WaitOne(TimeSpan.FromSeconds(10)));
             Assert.True(consumerClosed.WaitOne(TimeSpan.FromSeconds(10)));
         }
-        
+
+        [Fact]
+        public async Task Consumer_should_attach_to_specified_address()
+        {
+            var consumerAttached = new ManualResetEvent(false);
+            Attach attachFrame = null;
+
+            var testHandler = new TestHandler(@event =>
+            {
+                switch (@event.Id)
+                {
+                    case EventId.LinkRemoteOpen when @event.Context is Attach attach:
+                        attachFrame = attach;
+                        consumerAttached.Set();
+                        break;
+                }
+            });
+
+            using var host = new TestContainerHost(_address, testHandler);
+            host.Open();
+
+            await using var connection = await CreateConnection(_address);
+            await using var consumer = connection.CreateConsumer("test-consumer");
+
+            Assert.True(consumerAttached.WaitOne(TimeSpan.FromSeconds(10)));
+            Assert.IsType<Source>(attachFrame.Source);
+            Assert.Equal("test-consumer", ((Source) attachFrame.Source).Address);
+        }
         private static Task<IConnection> CreateConnection(string address)
         {
             var connectionFactory = new ConnectionFactory();
