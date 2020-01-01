19dfa928605e751ceeef4953c9b3ae172e0eacc5
Havret <h4vret@gmail.com>
2020-01-01 23:18:44 +0100

    Do not send any disposition frames until message is accepted

--- test/ActiveMQ.Net.Tests/ConsumerMessageAcknowledgementSpec.cs
+++ test/ActiveMQ.Net.Tests/ConsumerMessageAcknowledgementSpec.cs
@@ -9,6 +9,25 @@ namespace ActiveMQ.Net.Tests
     public class ConsumerMessageAcknowledgementSpec
     {
         [Fact]
+        public async Task Should_not_send_any_disposition_frames_until_message_is_accepted()
+        {
+            var address = AddressUtil.GetAddress();
+            using var host = new TestContainerHost(address);
+            host.Open();
+            
+            var messageSource = host.CreateMessageSource("a1");
+            await using var connection = await CreateConnection(address);
+            var consumer = await connection.CreateConsumerAsync("a1");
+            
+            messageSource.Enqueue(new Message("foo"));
+            var message = await consumer.ConsumeAsync();
+
+            Assert.NotNull(message);
+            var dispositionContext = messageSource.GetNextDisposition(TimeSpan.FromMilliseconds(100));
+            Assert.Null(dispositionContext);
+        }
+        
+        [Fact]
         public async Task Should_send_accepted_and_settled_disposition_frame_when_message_accepted()
         {
             var address = AddressUtil.GetAddress();
