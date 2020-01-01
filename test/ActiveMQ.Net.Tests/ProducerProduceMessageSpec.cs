283cb4fd3fe4125aaab490df382aea6f74975bc7
Havret <h4vret@gmail.com>
2020-01-01 12:40:44 +0100

    ProduceAsync should send unsettled message
    And wait for confirmation that the message was delivered

---
+++ test/ActiveMQ.Net.Tests/ProducerProduceMessageSpec.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ActiveMQ.Net.Tests.Utils;
+using Amqp.Handler;
+using Xunit;
+
+namespace ActiveMQ.Net.Tests
+{
+    public class ProducerProduceMessageSpec
+    {
+        [Fact]
+        public async Task Should_send_msg_and_wait_for_confirmation_from_the_server()
+        {
+            var address = AddressUtil.GetAddress();
+            var deliveryReceived = new ManualResetEvent(false);
+            var deliverySettled = false;
+
+            var testHandler = new TestHandler(@event =>
+            {
+                switch (@event.Id)
+                {
+                    case EventId.ReceiveDelivery when @event.Context is IDelivery delivery:
+                        deliverySettled = delivery.Settled;
+                        deliveryReceived.Set();
+                        break;
+                }
+            });
+
+            using var host = new TestContainerHost(address, testHandler);
+            host.Open();
+
+            await using var connection = await CreateConnection(address);
+            var producer = connection.CreateProducer("a1");
+
+            await producer.ProduceAsync(new Message("foo"));
+
+            Assert.True(deliveryReceived.WaitOne(TimeSpan.FromSeconds(10)));
+            Assert.False(deliverySettled);
+        }
+
+        private static Task<IConnection> CreateConnection(string address)
+        {
+            var connectionFactory = new ConnectionFactory();
+            return connectionFactory.CreateAsync(address);
+        }
+    }
+}
