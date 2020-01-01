7868a5a22e8dbaa847ba0996c9ed12bddb79bfb2
Havret <h4vret@gmail.com>
2020-01-01 12:10:02 +0100

    Add test message processor
    It required to be able to test Producer scenarios

---
+++ test/ActiveMQ.Net.Tests/Utils/MessageProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+using Amqp.Listener;
+
+namespace ActiveMQ.Net.Tests.Utils
+{
+    public class MessageProcessor : IMessageProcessor
+    {
+        private readonly ConcurrentQueue<Message> _messages = new ConcurrentQueue<Message>();
+
+        void IMessageProcessor.Process(MessageContext messageContext)
+        {
+            _messages.Enqueue(new Message(messageContext.Message));
+            messageContext.Complete();
+        }
+
+        int IMessageProcessor.Credit { get; } = 30;
+
+        public Message Dequeue()
+        {
+            if (_messages.TryDequeue(out  var message))
+            {
+                return message;
+            }
+
+            return null;
+        }
+    }
+}
