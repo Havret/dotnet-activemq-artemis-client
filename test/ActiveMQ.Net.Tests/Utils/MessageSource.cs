64fa086fce55e953f444333d23b316df086b11ea
Havret <h4vret@gmail.com>
2020-01-01 23:10:49 +0100

    Add option to get disposition status on MessageSource

--- test/ActiveMQ.Net.Tests/Utils/MessageSource.cs
+++ test/ActiveMQ.Net.Tests/Utils/MessageSource.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using Amqp.Listener;
 
@@ -7,10 +8,21 @@ namespace ActiveMQ.Net.Tests.Utils
     public class MessageSource : IMessageSource
     {
         private readonly ConcurrentQueue<Message> _messages = new ConcurrentQueue<Message>();
+        private readonly BlockingCollection<DispositionContext> _dispositions = new BlockingCollection<DispositionContext>();
 
         public void Enqueue(Message message)
         {
             _messages.Enqueue(message);
+        }
+
+        public DispositionContext GetNextDisposition(TimeSpan timeout)
+        {
+            if (_dispositions.TryTake(out var dispositionContext, timeout))
+            {
+                return dispositionContext;
+            }
+            
+            return default;
         }
 
         Task<ReceiveContext> IMessageSource.GetMessageAsync(ListenerLink link)
@@ -25,7 +37,7 @@ namespace ActiveMQ.Net.Tests.Utils
 
         void IMessageSource.DisposeMessage(ReceiveContext receiveContext, DispositionContext dispositionContext)
         {
-            
+            _dispositions.TryAdd(dispositionContext);
         }
     }
 }
