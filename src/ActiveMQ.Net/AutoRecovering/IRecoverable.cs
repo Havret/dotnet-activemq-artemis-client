72875c8e2f56a4cdf24363f4159beb3040b34d83
Havret <h4vret@gmail.com>
2020-01-05 17:15:42 +0100

    Recreate Consumers on connection recovery

---
+++ src/ActiveMQ.Net/AutoRecovering/IRecoverable.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ActiveMQ.Net.AutoRecovering
+{
+    internal delegate void Closed(IRecoverable recoverable);
+
+    internal interface IRecoverable
+    {
+        Task RecoverAsync(IConnection connection);
+        event Closed Closed;
+    }
+}
