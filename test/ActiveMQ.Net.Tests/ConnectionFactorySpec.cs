f9df8982291276f031ce25f70779101b7b9343ca
Havret <h4vret@gmail.com>
2020-02-02 19:36:36 +0100

    Create cancellable connection

--- test/ActiveMQ.Net.Tests/ConnectionFactorySpec.cs
+++ test/ActiveMQ.Net.Tests/ConnectionFactorySpec.cs
@@ -1,7 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using ActiveMQ.Net.AutoRecovering;
 using ActiveMQ.Net.Exceptions;
+using ActiveMQ.Net.Tests.Utils;
+using Amqp.Handler;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -46,6 +50,33 @@ namespace ActiveMQ.Net.Tests
 
             var exception = await Assert.ThrowsAsync<CreateConnectionException>(() => CreateConnection(Enumerable.Empty<Endpoint>()));
             Assert.Contains(exception.Message, "No endpoints provided.");
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task Should_cancel_CreateAsync_when_no_open_frame_received_within_specified_timeout(bool automaticRecoveryEnabled)
+        {
+            var endpoint = GetUniqueEndpoint();
+
+            var timeout = new ManualResetEvent(false);
+            var handler = new TestHandler(@event =>
+            {
+                switch (@event.Id)
+                {
+                    case EventId.ConnectionLocalOpen:
+                        timeout.WaitOne(Timeout);
+                        break;
+                }
+            });
+            using var host = CreateOpenedContainerHost(endpoint, handler);
+
+            var connectionFactory = CreateConnectionFactory();
+            connectionFactory.AutomaticRecoveryEnabled = automaticRecoveryEnabled;
+
+            var cts = new CancellationTokenSource(ShortTimeout);
+            await Assert.ThrowsAnyAsync<OperationCanceledException>(() => connectionFactory.CreateAsync(endpoint, cts.Token));
+            timeout.Set();
         }
     }
 }
