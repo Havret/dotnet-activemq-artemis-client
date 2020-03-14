d1654b60deb93b37e4d867f8591b7bb1f599071a
Havret <h4vret@gmail.com>
2020-03-14 17:07:21 +0100

    Support Application Properties

---
+++ src/ActiveMQ.Net/ApplicationProperties.cs
@@ -0,0 +1,18 @@
+﻿namespace ActiveMQ.Net
+{
+    public sealed class ApplicationProperties
+    {
+        private readonly Amqp.Framing.ApplicationProperties _innerProperties;
+
+        public ApplicationProperties(Amqp.Message innerMessage) 
+        {
+            _innerProperties = innerMessage.ApplicationProperties ??= new Amqp.Framing.ApplicationProperties();
+        }
+
+        public object this[string key]
+        {
+            get => _innerProperties.Map[key];
+            set => _innerProperties.Map[key] = value;
+        }
+    }
+}
