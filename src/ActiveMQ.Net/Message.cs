30df25777debe693ec41567a615038e3fb47c56d
Havret <h4vret@gmail.com>
2020-04-29 23:19:35 +0200

    Support all AMQP message payload types

--- src/ActiveMQ.Net/Message.cs
+++ src/ActiveMQ.Net/Message.cs
@@ -1,4 +1,8 @@
-﻿namespace ActiveMQ.Net
+﻿using System;
+using Amqp.Framing;
+using Amqp.Types;
+
+namespace ActiveMQ.Net
 {
     public class Message
     {
@@ -12,9 +16,42 @@
             InnerMessage = message;
         }
 
-        public Message(string body)
+        public Message(object body)
         {
-            InnerMessage = new Amqp.Message(body);
+            InnerMessage = new Amqp.Message
+            {
+                BodySection = GetBodySection(body)
+            };
+        }
+
+        private static RestrictedDescribed GetBodySection(object body)
+        {
+            switch (body)
+            {
+                case string _:
+                case char _:
+                case byte _:
+                case sbyte _:
+                case short _:
+                case ushort _:
+                case int _:
+                case uint _:
+                case long _:
+                case ulong _:
+                case float _:
+                case double _:
+                case Guid _:
+                case DateTime _:
+                    return new AmqpValue { Value = body };
+                case byte[] payload:
+                    return new Data { Binary = payload };
+                case List list:
+                    return new AmqpSequence { List = list };
+                case null:
+                    throw new ArgumentNullException(nameof(body));
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(body), $"The type '{body.GetType().FullName}' is not a valid AMQP type and cannot be encoded.");
+            }
         }
 
         public Properties Properties => _properties ??= new Properties(InnerMessage);
