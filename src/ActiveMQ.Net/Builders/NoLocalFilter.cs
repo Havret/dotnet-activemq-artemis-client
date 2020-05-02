252776e26e2286d977324757da76dcfd2d29f9da
Havret <h4vret@gmail.com>
2020-05-02 23:50:56 +0200

    NoLocal filter

---
+++ src/ActiveMQ.Net/Builders/NoLocalFilter.cs
@@ -0,0 +1,17 @@
+﻿using Amqp.Types;
+
+namespace ActiveMQ.Net.Builders
+{
+    internal class NoLocalFilter : DescribedValue
+    {
+        private static readonly ulong _descriptor = 0x0000468C00000003L;
+        private static readonly string _described = "NoLocalFilter{}";
+        public static readonly Symbol NoLocalFilterName = new Symbol("apache.org:no-local-filter:list");
+
+        private NoLocalFilter() : base(_descriptor, _described)
+        {
+        }
+
+        public static NoLocalFilter Instance { get; } = new NoLocalFilter();
+    }
+}
