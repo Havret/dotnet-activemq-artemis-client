a822b22a89cb81e799c826bf86a07d7839c294aa
Havret <h4vret@gmail.com>
2020-05-17 21:42:50 +0200

    Test message acknowledgement

--- test/ActiveMQ.Artemis.Client.IntegrationTests/MessageAcknowledgementSpec.cs
+++ test/ActiveMQ.Artemis.Client.IntegrationTests/MessageAcknowledgementSpec.cs
@@ -1,7 +1,71 @@
-﻿namespace ActiveMQ.Artemis.Client.IntegrationTests
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ActiveMQ.Artemis.Client.IntegrationTests
 {
-    public class MessageAcknowlegementSpec 
+    public class MessageAcknowledgementSpec : ActiveMQNetIntegrationSpec
     {
+        public MessageAcknowledgementSpec(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task Should_acknowledge_message()
+        {
+            await using var connection = await CreateConnection();
+            var address = Guid.NewGuid().ToString();
+            await using var producer = await connection.CreateAnonymousProducer();
+            var consumer1 = await connection.CreateConsumerAsync(address, QueueRoutingType.Anycast);
+
+            await producer.SendAsync(address, AddressRoutingType.Anycast, new Message("foo"));
+            var msg = await consumer1.ReceiveAsync();
+            await consumer1.AcceptAsync(msg);
+
+            await consumer1.DisposeAsync();
+
+            var consumer2 = await connection.CreateConsumerAsync(address, QueueRoutingType.Anycast);
+
+            // As message was accepted by consumer1, it shouldn't be redeliver to newly created consumer2
+            // after consumer1 was closed
+            await Assert.ThrowsAsync<OperationCanceledException>(async () => await consumer2.ReceiveAsync(new CancellationTokenSource(TimeSpan.FromMilliseconds(100)).Token));
+        }
+
+        [Fact]
+        public async Task Should_redeliver_message_to_the_same_consumer_when_message_rejected()
+        {
+            await using var connection = await CreateConnection();
+            var address = Guid.NewGuid().ToString();
+            await using var producer = await connection.CreateAnonymousProducer();
+            var consumer = await connection.CreateConsumerAsync(address, QueueRoutingType.Anycast);
+
+            await producer.SendAsync(address, AddressRoutingType.Anycast, new Message("foo"));
+            var msg = await consumer.ReceiveAsync();
+            consumer.Reject(msg);
+
+            msg = await consumer.ReceiveAsync(new CancellationTokenSource(TimeSpan.FromMilliseconds(500)).Token);
+            Assert.Equal("foo", msg.GetBody<string>());
+        }
         
+        [Fact]
+        public async Task Should_redeliver_message_to_different_consumer_when_message_rejected_with_undeliverableHere_flag_enabled()
+        {
+            await using var connection = await CreateConnection();
+            var address = Guid.NewGuid().ToString();
+            await using var producer = await connection.CreateAnonymousProducer();
+            var consumer1 = await connection.CreateConsumerAsync(address, QueueRoutingType.Anycast);
+
+            await producer.SendAsync(address, AddressRoutingType.Anycast, new Message("foo"));
+            var msg = await consumer1.ReceiveAsync();
+            consumer1.Reject(msg, undeliverableHere: true);
+            
+            await Assert.ThrowsAsync<OperationCanceledException>(async () => await consumer1.ReceiveAsync(new CancellationTokenSource(TimeSpan.FromMilliseconds(100)).Token));
+            
+            var consumer2 = await connection.CreateConsumerAsync(address, QueueRoutingType.Anycast);
+            msg = await consumer2.ReceiveAsync(new CancellationTokenSource(TimeSpan.FromMilliseconds(500)).Token);
+            Assert.Equal("foo", msg.GetBody<string>());
+        }
     }
 }
