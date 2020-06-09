7a513072e5d35b1ef8eb53f03212685c88263494
Havret <h4vret@gmail.com>
2020-06-09 22:49:41 +0200

    Get free port for tests instead of choosing it randomly

--- test/ActiveMQ.Artemis.Client.UnitTests/Utils/EndpointUtil.cs
+++ test/ActiveMQ.Artemis.Client.UnitTests/Utils/EndpointUtil.cs
@@ -1,20 +1,23 @@
-﻿using System;
+﻿using System.Net;
+using System.Net.Sockets;
 
 namespace ActiveMQ.Artemis.Client.UnitTests.Utils
 {
     public static class EndpointUtil
     {
-        private static int _port = 10000;
-        private static readonly object _syncRoot = new object();
-
         public static Endpoint GetUniqueEndpoint()
         {
-            lock (_syncRoot)
-            {
-                var random = new Random();
-                _port += random.Next(1, 10);
-                return Endpoint.Create("localhost", _port, "guest", "guest");
-            }
+            var port = GetFreePort();
+            return Endpoint.Create("localhost", port, "guest", "guest");
+        }
+
+        private static int GetFreePort()
+        {
+            var tcpListener = new TcpListener(IPAddress.Loopback, 0);
+            tcpListener.Start();
+            var port = ((IPEndPoint)tcpListener.LocalEndpoint).Port;
+            tcpListener.Stop();
+            return port;
         }
     }
 }
