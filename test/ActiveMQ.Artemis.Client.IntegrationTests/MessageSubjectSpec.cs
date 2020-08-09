83d479e2af51fe5c5978dd92a4898ceadddda6ed
Havret <h4vret@gmail.com>
2020-08-09 21:49:54 +0200

    Add support for subject field

---
+++ test/ActiveMQ.Artemis.Client.IntegrationTests/MessageSubjectSpec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ActiveMQ.Artemis.Client.IntegrationTests
+{
+    public class MessageSubjectSpec : ActiveMQNetIntegrationSpec
+    {
+        public MessageSubjectSpec(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task Should_send_message_with_subject_specified()
+        {
+            var address = Guid.NewGuid().ToString();
+            await using var connection = await CreateConnection();
+            await using var producer = await connection.CreateProducerAsync(address, RoutingType.Anycast);
+
+            await producer.SendAsync(new Message("foo") { Subject = "MyCustomSubject" });
+
+            await using var consumer = await connection.CreateConsumerAsync(address, RoutingType.Anycast);
+
+            Assert.Equal("MyCustomSubject", (await consumer.ReceiveAsync(CancellationToken)).Subject);
+        }
+    }
+}
