4432f66aa52cfff2dc26a77d0be02c6beb67eec0
Havret <h4vret@gmail.com>
2020-08-09 22:07:30 +0200

    Support for reply-to field

---
+++ test/ActiveMQ.Artemis.Client.IntegrationTests/MessageReplyToSpec.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ActiveMQ.Artemis.Client.IntegrationTests
+{
+    public class MessageReplyToSpec : ActiveMQNetIntegrationSpec
+    {
+        public MessageReplyToSpec(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task Should_send_message_with_the_address_of_the_node_to_send_replied_to()
+        {
+            var toAddress = Guid.NewGuid().ToString();
+            var replyToAddress = Guid.NewGuid().ToString();
+
+            await using var connection = await CreateConnection();
+            await using var producer = await connection.CreateProducerAsync(toAddress, RoutingType.Anycast);
+            await using var replyRecipient = await connection.CreateConsumerAsync(replyToAddress, RoutingType.Anycast);
+
+            await producer.SendAsync(new Message("request") { ReplyTo = replyToAddress }, CancellationToken);
+
+            await using var consumer = await connection.CreateConsumerAsync(toAddress, RoutingType.Anycast);
+            var requestMsg = await consumer.ReceiveAsync(CancellationToken);
+
+            await using var anonymousProducer = await connection.CreateAnonymousProducerAsync();
+            await anonymousProducer.SendAsync(requestMsg.ReplyTo, RoutingType.Anycast, new Message("reply"), CancellationToken);
+
+            Assert.Equal("reply", (await replyRecipient.ReceiveAsync()).GetBody<string>());
+        }
+    }
+}
