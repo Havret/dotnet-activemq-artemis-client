8f07b42b74bf760e319233f0501e40782bfd77c9
Havret <h4vret@gmail.com>
2020-08-17 22:44:32 +0200

    Add TryGetValue<T> and ContainsKey methods to ApplicationProperties API

--- src/ActiveMQ.Artemis.Client/ApplicationProperties.cs
+++ src/ActiveMQ.Artemis.Client/ApplicationProperties.cs
@@ -4,7 +4,7 @@
     {
         private readonly Amqp.Framing.ApplicationProperties _innerProperties;
 
-        public ApplicationProperties(Amqp.Message innerMessage) 
+        public ApplicationProperties(Amqp.Message innerMessage)
         {
             _innerProperties = innerMessage.ApplicationProperties ??= new Amqp.Framing.ApplicationProperties();
         }
@@ -15,7 +15,20 @@
             set => _innerProperties.Map[key] = value;
         }
 
-        internal bool TryGetValue<T>(string key, out T value)
+        /// <summary>
+        /// Determines whether the <see cref="ActiveMQ.Artemis.Client.ApplicationProperties"></see> contains the specified key.
+        /// </summary>
+        /// <param name="key">The key to locate in the <see cref="ActiveMQ.Artemis.Client.ApplicationProperties"></see>.</param>
+        /// <returns>true if the <see cref="ActiveMQ.Artemis.Client.ApplicationProperties"></see> contains a property with the specified key; otherwise, false.</returns>
+        public bool ContainsKey(string key) => _innerProperties.Map.ContainsKey(key);
+
+        /// <summary>
+        /// Gets the property associated with the specified key.
+        /// </summary>
+        /// <param name="key">The key of the property to get.</param>
+        /// <param name="value">When this method returns, contains the property associated with the specified key, if the key is found; otherwise, the default value for the type of the value parameter. This parameter is passed uninitialized.</param>
+        /// <returns>true if the <see cref="ActiveMQ.Artemis.Client.ApplicationProperties"></see> contains a property with the specified key; otherwise, false.</returns>
+        public bool TryGetValue<T>(string key, out T value)
         {
             if (_innerProperties.Map.TryGetValue(key, out var objectValue) && objectValue is T typedValue)
             {
