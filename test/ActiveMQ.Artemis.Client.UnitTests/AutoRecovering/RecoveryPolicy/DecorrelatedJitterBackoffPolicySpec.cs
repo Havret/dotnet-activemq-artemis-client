086677931a4b6470e92966e9da995cd96b87b5bb
Havret <h4vret@gmail.com>
2021-05-30 10:35:44 +0200

    DecorrelatedJitterBackoffRecoveryPolicy should not overflow to give negative timespan

--- test/ActiveMQ.Artemis.Client.UnitTests/AutoRecovering/RecoveryPolicy/DecorrelatedJitterBackoffPolicySpec.cs
+++ test/ActiveMQ.Artemis.Client.UnitTests/AutoRecovering/RecoveryPolicy/DecorrelatedJitterBackoffPolicySpec.cs
@@ -14,7 +14,7 @@ namespace ActiveMQ.Artemis.Client.UnitTests.AutoRecovering.RecoveryPolicy
             var recoveryPolicy = RecoveryPolicyFactory.DecorrelatedJitterBackoff(TimeSpan.FromSeconds(1), retryCount);
             Assert.Equal(10, recoveryPolicy.RetryCount);
         }
-        
+
         [Fact]
         public void Should_return_no_limit_retry_count_when_no_retry_count_specified()
         {
@@ -52,8 +52,8 @@ namespace ActiveMQ.Artemis.Client.UnitTests.AutoRecovering.RecoveryPolicy
                 }
             }
         }
-        
-        
+
+
         [Fact]
         public void Should_return_delay_in_range()
         {
@@ -75,7 +75,7 @@ namespace ActiveMQ.Artemis.Client.UnitTests.AutoRecovering.RecoveryPolicy
                 AssertOnRetryDelayForTry(t, timeSpan, medianFirstDelay);
             }
         }
-        
+
         [Fact]
         public void Should_return_delay_in_range_wide()
         {
@@ -87,24 +87,24 @@ namespace ActiveMQ.Artemis.Client.UnitTests.AutoRecovering.RecoveryPolicy
 
             // Act
             var recoveryPolicy = RecoveryPolicyFactory.DecorrelatedJitterBackoff(medianFirstRetryDelay: medianFirstDelay, retryCount: retryCount, seed: seed, fastFirst: fastFirst);
-            var timeSpans = Enumerable.Range(1, retryCount).Select(x => recoveryPolicy.GetDelay(x)).ToList();
+            var delays = Enumerable.Range(1, retryCount).Select(x => recoveryPolicy.GetDelay(x)).ToList();
 
             // Assert
             int t = 0;
-            foreach (var timeSpan in timeSpans)
+            foreach (var delay in delays)
             {
                 t++;
-                AssertOnRetryDelayForTry(t, timeSpan, medianFirstDelay);
+                AssertOnRetryDelayForTry(t, delay, medianFirstDelay);
             }
         }
-        
+
         [Fact]
         public void Should_throw_exception_when_median_first_retry_delay_less_than_zero()
         {
             var delay = TimeSpan.FromMilliseconds(-1);
             Assert.Throws<ArgumentOutOfRangeException>(() => RecoveryPolicyFactory.DecorrelatedJitterBackoff(delay));
         }
-        
+
         [Fact]
         public void Should_throw_exception_when_retry_count_less_than_zero()
         {
@@ -113,10 +113,31 @@ namespace ActiveMQ.Artemis.Client.UnitTests.AutoRecovering.RecoveryPolicy
             Assert.Throws<ArgumentOutOfRangeException>(() => RecoveryPolicyFactory.DecorrelatedJitterBackoff(delay, retryCount: retryCount));
         }
 
+        [Fact]
+        public void Should_not_overflow_to_give_negative_timespan()
+        {
+            const int retryCount = 50;
+            var recoveryPolicy = RecoveryPolicyFactory.DecorrelatedJitterBackoff(TimeSpan.FromSeconds(1), retryCount: retryCount);
+            var delays = Enumerable.Range(1, retryCount).Select(x => recoveryPolicy.GetDelay(x));
+
+            Assert.All(delays, delay => Assert.True(delay > TimeSpan.Zero));
+        }
+
+        [Fact]
+        public void Should_not_return_delay_longer_than_specified_max_delay()
+        {
+            const int retryCount = 50;
+            var maxDelay = TimeSpan.FromSeconds(30);
+            var recoveryPolicy = RecoveryPolicyFactory.DecorrelatedJitterBackoff(medianFirstRetryDelay: TimeSpan.FromSeconds(1), maxDelay: maxDelay, retryCount: retryCount);
+            var delays = Enumerable.Range(1, retryCount).Select(x => recoveryPolicy.GetDelay(x));
+            
+            Assert.Equal(maxDelay, delays.Max());
+        }
+
         private static void AssertOnRetryDelayForTry(int t, TimeSpan calculatedDelay, TimeSpan medianFirstDelay)
         {
             Assert.True(calculatedDelay >= TimeSpan.Zero);
-            int upperLimitFactor = t < 2 ? (int)Math.Pow(2, t + 1) : (int)(Math.Pow(2, t + 1) - Math.Pow(2, t - 1));
+            int upperLimitFactor = t < 2 ? (int) Math.Pow(2, t + 1) : (int) (Math.Pow(2, t + 1) - Math.Pow(2, t - 1));
             Assert.True(calculatedDelay <= TimeSpan.FromTicks(medianFirstDelay.Ticks * upperLimitFactor));
         }
     }
