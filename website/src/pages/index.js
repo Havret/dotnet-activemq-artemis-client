b517c7408686789afe1c944506a7cfc24a680fe5
Havret <h4vret@gmail.com>
2022-01-06 19:53:29 +0100

    Clean up the docs title

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -56,8 +56,8 @@ function Home() {
   const { siteConfig = {} } = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description="Unofficial ActiveMQ Artemis .NET Client for .NET Core and .NET Framework">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
