525398ec408c76e9e85cc98f7c27cc51abb1c758
Havret <h4vret@gmail.com>
2023-01-17 23:15:36 +0100

    Invoke ConfigureTopology before message producers are created

--- test/ArtemisNetClient.Extensions.IntegrationTests/ConfigureTopologySpec.cs
+++ test/ArtemisNetClient.Extensions.IntegrationTests/ConfigureTopologySpec.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using ActiveMQ.Artemis.Client.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -38,5 +39,44 @@ public class ConfigureTopologySpec
         var queueNames = await topologyManager.GetQueueNamesAsync(testFixture.CancellationToken);
 
         Assert.Contains(queueName, queueNames);
+    }
+
+    [Fact]
+    public async Task Should_declare_queue_with_last_value_key_when_anycast_producer_is_registered_for_that_queue()
+    {
+        var addressName = Guid.NewGuid().ToString();
+
+        await using var testFixture = await TestFixture.CreateAsync(_testOutputHelper, activeMqBuilder =>
+        {
+            activeMqBuilder.ConfigureTopology(async (_, topologyManager) =>
+                {
+                    await topologyManager.DeclareQueueAsync(new QueueConfiguration
+                    {
+                        Address = addressName,
+                        Name = addressName,
+                        RoutingType = RoutingType.Anycast,
+                        AutoCreateAddress = true,
+                        LastValueKey = "my-key"
+                    });
+                })
+                .AddProducer<TestProducer>(address: addressName, RoutingType.Anycast);
+        });
+
+        // Send two messages with the same LastValueKey
+        var producer = testFixture.Services.GetRequiredService<TestProducer>();
+        await producer.Producer.SendAsync(new Message("foo") { ApplicationProperties = { ["my-key"] = "1" } });
+        await producer.Producer.SendAsync(new Message("bar") { ApplicationProperties = { ["my-key"] = "1" } });
+        
+        // Verify that LastValueKey feature was applied for the queue
+        // Only the last message should be available for the consumer
+        await using var consumer = await testFixture.Connection.CreateConsumerAsync(address: addressName, RoutingType.Anycast);
+        var msg = await consumer.ReceiveAsync(testFixture.CancellationToken);
+        Assert.Equal("bar", msg.GetBody<string>());
+    }
+    
+    private class TestProducer
+    {
+        public IProducer Producer { get; }
+        public TestProducer(IProducer producer) => Producer = producer;
     }
 }
