39cc2633fb6d913b3480a98d4acdf14abef02915
Havret <h4vret@gmail.com>
2023-08-07 23:14:10 +0200

    Revert "Simplify TestKit implementation"
    This reverts commit bf7922e4be1c8cd6865c805ba4c8ccf96a307153.
    
    # Conflicts:
    #	src/ArtemisNetClient.Testing/Listener/Extensions.cs

--- src/ArtemisNetClient.Testing/Listener/Extensions.cs
+++ src/ArtemisNetClient.Testing/Listener/Extensions.cs
@@ -1,4 +1,6 @@
 using System.Reflection;
+using ActiveMQ.Artemis.Client.Testing.Utils;
+using Amqp;
 using Amqp.Framing;
 using Amqp.Listener;
 
@@ -6,10 +8,41 @@ namespace ActiveMQ.Artemis.Client.Testing.Listener;
 
 internal static class Extensions
 {
+    private static bool IsDetaching(this Link link)
+    {
+        return link.LinkState >= LinkState.DetachPipe;
+    }
+
+    public static void AddSafeClosed(this Link link, ClosedCallback onLinkClosed)
+    {
+        link.AddClosedCallback(onLinkClosed);
+        if (link.IsDetaching())
+        {
+            onLinkClosed(link, link.Error);
+        }
+    }
+
+    public static void InitializeLinkEndpoint(this ListenerLink link, LinkEndpoint linkEndpoint, uint credit)
+    {
+        var type = typeof(TargetLinkEndpoint);
+        var methodInfo = type.GetMethod("InitializeLinkEndpoint")!;
+        methodInfo.Invoke(link, new object[] {linkEndpoint, credit});
+    }
+
     public static void Dispose(this MessageContext messageContext, DeliveryState deliveryState)
     {
         var type = typeof(Context);
         var methodInfo = type.GetMethod("Dispose", BindingFlags.Instance | BindingFlags.NonPublic);
         methodInfo!.Invoke(messageContext, new object[] {deliveryState});
+    }
+
+    public static void SetSettleOnSend(this ListenerLink link, bool b)
+    {
+        ReflectionUtils.SetPropValue(link, "SettleOnSend", true);
+    }
+
+    public static string GetRemoteContainerId(this ListenerConnection connection)
+    {
+        return ReflectionUtils.GetPropValue<string>(connection, "RemoteContainerId")!;
     }
 }
