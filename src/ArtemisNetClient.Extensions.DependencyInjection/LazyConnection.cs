9a0862b0f64ec1f23fa655909786f02af856fec5
Havret <h4vret@gmail.com>
2025-07-20 16:20:27 +0200

    Refactor ConnectionProvider to use KeyedService api

---
+++ src/ArtemisNetClient.Extensions.DependencyInjection/LazyConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ActiveMQ.Artemis.Client.Extensions.DependencyInjection.InternalUtils;
+
+namespace ActiveMQ.Artemis.Client.Extensions.DependencyInjection;
+
+/// <summary>
+/// Represents a lazily initialized connection to an ActiveMQ Artemis broker.
+/// </summary>
+internal class LazyConnection(Func<CancellationToken, Task<IConnection>> factory)
+{
+    /// <summary>
+    /// Gets the lazily initialized connection.
+    /// </summary>
+    public AsyncValueLazy<IConnection> Connection { get; } = new(factory);
+}
