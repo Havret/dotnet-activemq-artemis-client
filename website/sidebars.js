94fbe675d597b3e7a3ab8f72c4c1b6dc9047715b
Havret <h4vret@gmail.com>
2026-01-30 00:33:31 +0100

    Add documentation for message filtering and NoLocal filter

--- website/sidebars.js
+++ website/sidebars.js
@@ -6,6 +6,7 @@ module.exports = {
       'message-durability',
       'message-priority',
       'consumer-credit',
+      'message-filtering',
       'connection-lifecycle',
       'auto-recovery',
       'transactions',
